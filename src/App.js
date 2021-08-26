e5812f23540fa21128bb5534d9ac31e52a20a0dc
rida jamil <rida.jamil7586@gmail.com>
2021-08-26 23:15:27 +0500

    initial files

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { useState } from 'react';
+import './App.css';
+import Cards from './Components/Cards'
+
+
+const App = () => {
+  const [user, setUser] = useState([])
+
+  return(
+    <>
+    </>
+  )
+}
+
+export default App;
