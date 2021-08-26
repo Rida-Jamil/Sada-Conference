6801f499b5b252844489c51e414d89f97cac3366
rida jamil <rida.jamil7586@gmail.com>
2021-08-26 23:59:09 +0500

    random check

--- src/App.js
+++ src/App.js
@@ -1,15 +1,38 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
-import Cards from './Components/Cards'
+import Cards from './Components/Cards/Cards';
+
 
 
 const App = () => {
-  const [user, setUser] = useState([])
 
-  return(
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    fetch("https://randomuser.me/api/?results=10")
+      .then(response => response.json())
+      .then(data => {
+        console.log(data);
+        setUsers(data.results);
+      });
+  }, [])
+
+
+
+  return (
     <>
+      {users.map(user => (
+        <Cards
+          image={user.picture.medium}
+          name={user.name.first + " " + user.name.last}
+          email={user.email}
+          nationality={user.nat}
+          age={user.dob.age}
+        />
+      ))}
     </>
-  )
+  );
+
 }
 
 export default App;
--- src/Components/Cards/Cards.js
+++ src/Components/Cards/Cards.js
@@ -1,23 +1,30 @@
-import React from 'react';
-import Card from 'react-bootstrap/Card'
-import Button from 'react-bootstrap/Button'
+import React, { useState } from 'react';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
 
-const Cards = () => {
+
+const Cards = (props) => {
+
+    const [age, setAge] = useState(false);
+
     return (
-        <>
+        <div className="container">
             <Card style={{ width: '18rem' }}>
-                <Card.Img variant="top" src="holder.js/100px180" />
+                <Card.Img variant="top" src= {props.image} />
                 <Card.Body>
-                    <Card.Title>Card Title</Card.Title>
+                    <Card.Title>Contact Name</Card.Title>
                     <Card.Text>
-                        Some quick example text to build on the card title and make up the bulk of
-                        the card's content.
-    </Card.Text>
-                    <Button variant="primary">Go somewhere</Button>
+                        <p>Name: {props.name} </p>
+                        <p>Email: {props.email} </p>
+                        <p>Nationality: {props.nationality} </p>
+                        {age && <p> Age: {props.age} </p>}
+                    </Card.Text>
+                    <Button variant="success" onClick={() => setAge(!age)}>Show Age</Button>
                 </Card.Body>
             </Card>
-        </>
-    )
-}
+        </div>
+    );
+};
+
 
 export default Cards;
