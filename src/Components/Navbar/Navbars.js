2d7d9a453cd7df243e61cf8d74607f2083ea36ce
Rida Jamil <rida.jamil7586@gmail.com>
2021-08-28 14:34:08 +0500

    navButton

--- src/Components/Navbar/Navbars.js
+++ src/Components/Navbar/Navbars.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import { Navbar, Container, Nav } from 'react-bootstrap';
+import { Button, Navbar, Container } from 'react-bootstrap';
 import './Navbar.css';
 import Home from '../Home/Home';
 import Contact from '../Contact/Contact';
 import About from '../About/About';
 import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
 } from "react-router-dom";
 
 const Navbars = () => {
@@ -16,19 +16,23 @@ const Navbars = () => {
         <>
             <Router>
                 <div>
-                    <Navbar bg="dark" variant="dark">
+                   <Navbar bg="dark" variant="dark">
                         <Container>
-                                <Navbar.Brand as={Link} to="/" >Sada-Event</Navbar.Brand>
-                            <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                            <Navbar.Collapse id="basic-navbar-nav">
-                                <Nav className="me-auto">
-                                        <Nav.Link as={Link} to="/Home" > Home </Nav.Link>
-                                        <Nav.Link as={Link} to="/About" > About </Nav.Link>
-                                        <Nav.Link as={Link} to="/Contact" > Contact </Nav.Link>
-                                </Nav>
-                            </Navbar.Collapse>
+                            <Navbar.Brand as={Link} to="/" >Sada-Event</Navbar.Brand>
                         </Container>
                     </Navbar>
+                    
+                    <div className="d-grid gap-2 nav-btn">
+                        <Button variant="primary" size="lg" as={Link} to="/Home" >
+                            Home button
+                        </Button>
+                        <Button variant="secondary" size="lg" as={Link} to="/About" >
+                            About button
+                        </Button>
+                        <Button variant="secondary" size="lg" as={Link} to="/Contact" >
+                            Contact button
+                        </Button>
+                    </div>
                     {/* A <Switch> looks through its children <Route>s and
             renders the first one that matches the current URL. */}
                     <Switch>
